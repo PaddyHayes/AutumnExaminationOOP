14b301a4bded19cb3beb4568b3445560e2247b7d
PaddyHayes <hayespaddy96@gmail.com>
2020-08-31 11:40:31 +0100

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        public wineAge(){
+        wineAge[] ageWine = new wineAge[256];
+
+        for(int i = 0; i<= ageWine.length; i++) {
+
+            JOptionPane.showInputDialog("Please enter a brand name: " + )
+            JOptionPane.showConfirmDialog(null, "Objects entered: " + ageWine[i], "Objects", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
---
+++ src/wineAge.java
@@ -0,0 +1,38 @@
+public class wineAge {
+
+    private int age;
+    String brand;
+    int price;
+
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+
+    @Override
+    public String toString() {
+        return "wineAge{" +
+                "age=" + age +
+                '}';
+    }
+
+    public wineAge(String winebrand, int wineprice, int wineage){
+        brand = winebrand;
+        age = wineage;
+        price = wineprice;
+
+    }
+}/*(d) Question 1The OOP concept of absraction is only showing the essential attributes and will hide information that is not needed for the user.
+It helps to reduce the complexity for programming.
+An object of an abstract cannot be created by using a new key word.
+It can only be instantiated by using a concrete subclass or defining the abstract method.
+A class can be instantiated by directly using a new keyword*/
+
+/*(e) Encapsulation is the wrapping of data into a single unit e.g. it puts code together.
+Data abstraction simplifies a body of data by reducing it.
+Polymorphism will let an object take on other forms for example when a parent class is refered to a child class object.
+Inheritance in Java is creating new class one already existing ones so that you can reuse methods.
+ */
